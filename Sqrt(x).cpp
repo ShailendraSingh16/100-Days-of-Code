5a2b8914a0256dac4c8afaea8e0f99d2f8d25b8a
Shailendra Singh <75472269+ShailendraSingh16@users.noreply.github.com>
2022-12-31 17:44:00 +0530

    Create Sqrt(x).cpp

---
+++ Sqrt(x).cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int s=0;
+        int e=x;
+        
+        long long int mid =s+(e-s)/2;
+        
+        long long int ans= -1;
+        while(s<=e){
+            long long int square=mid*mid;
+            
+            if(square==x){
+                return mid;
+            }
+            if(square<x){
+                ans=mid;
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+            mid=s+(e-s)/2;
+        }
+        return ans;
+    }
+};
