3331527ecdc18d2ea12affdb08bb95794bbe58cc
Shailendra Singh <75472269+ShailendraSingh16@users.noreply.github.com>
2023-01-24 23:25:38 +0530

    Update ans.cpp

--- 55. Jump Game/ans.cpp
+++ 55. Jump Game/ans.cpp
@@ -1,13 +1,16 @@
-class Solution {
+ class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n=nums.size();
+         int n = nums.size()-1;
 
         for(int i=nums.size()-1;i>=0;i--){
-            if(nums[i]+i>=n){
+            if(i+nums[i]>=n){
                 n=i;
             }
         }
-        return n==0;
+
+        if(n==0) return true;
+        
+        else return false;
     }
 };
