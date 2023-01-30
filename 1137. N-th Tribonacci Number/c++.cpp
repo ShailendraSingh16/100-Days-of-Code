11e10943fe30677c55dc70e49a7bd84d113715b2
Shailendra Singh <75472269+ShailendraSingh16@users.noreply.github.com>
2023-01-30 17:43:12 +0530

    Create c++.cpp

---
+++ 1137. N-th Tribonacci Number/c++.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int t1=0,t2=1,t3=1;
+        int ans;
+
+        if(n==0){
+            return 0; 
+        }
+
+        if(n==1 || n==2){
+            return 1;  
+        }
+
+        for(int i=3;i<=n;i++){  
+            ans = t1 + t2 + t3; 
+            t1 = t2;  
+            t2 = t3;  
+            t3 = ans;  
+        }
+        return ans;
+    }
+};
