f8eb911f91282114d57f8c1ae39ae1d6f56dd7a1
Shailendra Singh <75472269+ShailendraSingh16@users.noreply.github.com>
2023-04-14 12:36:43 +0530

    Create subSequence.cpp

---
+++ Subsequences of String/subSequence.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+
+void solve(string str,string output, int index, vector<string>& ans){
+	
+	if(index>=str.length()){
+		if(output.length()>0){
+		ans.push_back(output);
+		}
+		return;
+	}
+
+	solve(str,output,index+1,ans);
+
+	char elemenet=str[index];
+	output.push_back(elemenet);
+	solve(str,output,index+1,ans);
+}
+vector<string> subsequences(string str){
+	
+	vector<string>ans;
+	string output="";
+	int index=0;
+
+	solve(str,output,index,ans);
+
+	return ans;
+}
